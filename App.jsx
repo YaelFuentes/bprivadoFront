250761afa363c57c599c4134decf4a2d96fe7eea
YaelFuentes <yaelfuentes32@gmail.com>
2023-02-28 10:50:59 -0300

    commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable react/react-in-jsx-scope */
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  console.log('hola mundo')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app now!</Text>
       <StatusBar style="auto" />
     </View>
   );
